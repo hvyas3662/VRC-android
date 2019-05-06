c6474c98a2104bf637d02f589438cfb620b041f0
Harshal <hitesh.vyas3662@gmail.com>
2019-05-06 23:58:45 +0530

    change minimum sdk version 21 to 16

--- app/src/main/java/com/hvyas/sample/MainActivity.java
+++ app/src/main/java/com/hvyas/sample/MainActivity.java
@@ -3,14 +3,11 @@ package com.hvyas.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.hvyas.volleyrequestconverter.VRC;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        VRC v = new VRC();
     }
 }
