16bbe225ff00bc0228f713de2681b257bed57b46
Harshal vyas <43903754+hvyas3662@users.noreply.github.com>
2022-06-05 13:30:02 +0530

    Migrate to AndroidX Convert whole lib in kotlin

--- volleyrequestconverter/src/androidTest/java/com/hvyas/volleyrequestconverter/ExampleInstrumentedTest.java
+++ volleyrequestconverter/src/androidTest/java/com/hvyas/volleyrequestconverter/ExampleInstrumentedTest.java
@@ -1,13 +1,7 @@
 package com.hvyas.volleyrequestconverter;
 
-import static org.junit.Assert.assertEquals;
-
-import android.content.Context;
-
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -17,11 +11,4 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.volleyrequestconverter.test", appContext.getPackageName());
-    }
 }
