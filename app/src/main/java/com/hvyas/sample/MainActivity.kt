79ef715eb20960014995ce38053cfff2bbff7863
Harshal vyas <43903754+hvyas3662@users.noreply.github.com>
2022-06-05 13:47:14 +0530

    App Converted To Kotlin Lib name changed

--- app/src/main/java/com/hvyas/sample/MainActivity.kt
+++ app/src/main/java/com/hvyas/sample/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.hvyas.sample;
+package com.hvyas.sample
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
