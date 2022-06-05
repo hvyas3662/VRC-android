8c3eec25cdd086291ac678ed5dd42ecec7c4bc43
Harshal vyas <43903754+hvyas3662@users.noreply.github.com>
2022-06-05 13:29:28 +0530

    Migrate to AndroidX Convert whole lib in kotlin

--- volleyrequestconverter/src/main/java/com/hvyas/volleyrequestconverter/VRC.kt
+++ volleyrequestconverter/src/main/java/com/hvyas/volleyrequestconverter/VRC.kt
@@ -1,34 +1,21 @@
-package com.hvyas.volleyrequestconverter;
+package com.hvyas.volleyrequestconverter
 
-import java.util.HashMap;
-import java.util.Map;
+@Suppress("unused")
+open class VRC(private val url: String, private val payload: Map<String, String>) {
 
-public class VRC {
-    private String url = "";
-    private Map<String, String> payload = new HashMap<>();
-
-    public VRC(String url, Map<String, String> payload) {
-        this.url = url;
-        this.payload.putAll(payload);
-    }
-
-    public String getRequestedUrl() {
-        String new_url = url + "?";
-        for (int i = 0; i < payload.size(); i++) {
-            String key = (payload.keySet().toArray())[i].toString();
-            new_url = new_url + key + "=" + payload.get(key) + "&";
+    fun getRequestedUrl(): String {
+        var newUrl = "$url?"
+        for (i in 0 until payload.size) {
+            val key = payload.keys.toTypedArray()[i]
+            newUrl = newUrl + key + "=" + payload[key] + "&"
         }
-        new_url = new_url.substring(0, new_url.length() - 1);
-        return new_url;
+        newUrl = newUrl.substring(0, newUrl.length - 1)
+        return newUrl
     }
 
-
-    public String getParams(String key) {
-        String value = payload.get(key);
-        if (value == null) {
-            return "Key not exist";
-        } else {
-            return value;
-        }
+    fun getParams(key: String): String {
+        val value = payload[key]
+        return value ?: "Key not exist"
     }
+
 }
