af0b4dc97785b55ffa0a972f09207fe20e795c85
Harshal vyas <43903754+hvyas3662@users.noreply.github.com>
2022-06-05 14:43:09 +0530

    Package changed hvyas to elevationsoft

--- app/src/androidTest/java/com/elevationsoft/sample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/elevationsoft/sample/ExampleInstrumentedTest.java
@@ -1,11 +1,6 @@
 package com.elevationsoft.sample;
 
-import static org.junit.Assert.assertEquals;
-
-import android.content.Context;
-
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,9 +14,6 @@ import org.junit.runner.RunWith;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.sample", appContext.getPackageName());
     }
 }
