6673bc3470df491ca5577e625ae0654dfec991b1
Harshal vyas <43903754+hvyas3662@users.noreply.github.com>
2022-06-05 14:53:38 +0530

    Example Added

--- app/src/main/java/com/elevationsoft/sample/MainActivity.kt
+++ app/src/main/java/com/elevationsoft/sample/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.elevationsoft.sample
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.elevationsoft.vrclib.Vrc
+import com.hvyas.sample.R
 
 class MainActivity : AppCompatActivity() {
+    private var tvUrl: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val et = findViewById<EditText>(R.id.et_search)
+        tvUrl = findViewById(R.id.tv_url)
+
+        et.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setSearchUrl(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
+
+    }
+
+    private fun setSearchUrl(searchQuery: String) {
+        val url = "http://Example.com/android/api/abcd.php"
+        val params: MutableMap<String, String> = HashMap()
+        params["search"] = searchQuery
+        val fullUrl: String = Vrc(url, params).getRequestedUrl()
+        tvUrl?.text = fullUrl
     }
 }
